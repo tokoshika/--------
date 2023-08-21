a735d59ab5d4519cfebf579fed9f942fe10c0c46
pythonthm <pythonthm@gmail.com>
2023-08-22 08:21:48 +0900

    first comitte

---
+++ allback/layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav>
+    <ul class="nav">
+      <li class="color">
+        <NuxtLink to="/" style="color: rgb(48, 159, 38); text-decoration: none"
+          >リコーダー</NuxtLink
+        >
+      </li>
+      <li>
+        <NuxtLink to="/youtube" style="color: red; text-decoration: none"
+          >YouTube</NuxtLink
+        >
+      </li>
+    </ul>
+  </nav>
+
+  <slot />
+</template>
+
+<style>
+  .nav {
+    display: flex;
+    list-style: none;
+  }
+
+  li {
+    margin-left: 0.3rem;
+    margin-right: 0.3rem;
+  }
+
+  .color {
+    text-decoration: none;
+  }
+</style>
---
+++ allback/pages/youtube.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="main-container">
+    <h1>this is youtube.vue</h1>
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/61QbMng2XnA"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    ></iframe>
+  </div>
+</template>
+
+<style>
+  .main-container {
+    justify-content: center;
+    text-align: center;
+  }
+</style>
