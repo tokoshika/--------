97e1f25b878e33551005fc08efa06226c3e7bf36
pythonthm <pythonthm@gmail.com>
2023-09-06 10:55:36 +0900

    home

--- allback/pages/index.vue
+++ allback/pages/index.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>
-    <h1>強風オールバック</h1>
+  <div class="main-container">
+    <div class="title">
+      <h1>鹿追小学校6年生</h1>
+      <h2>リコーダー練習用</h2>
+    </div>
+    <img class="kyofu" src="~/assets/kyufu-thanks.png" />
   </div>
 </template>
+
+<style scoped>
+  .main-container {
+    display: flex;
+    justify-content: center;
+    margin-left: 0;
+  }
+  .title{
+    margin-right:1rem;
+  }
+  .kyofu {
+    width: 55%;
+    display: flex;
+    justify-content: center;
+  }
+</style>
