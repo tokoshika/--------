fb60ca3c8ec0b939e168c310fae817215cf8bb25
pythonthm <pythonthm@gmail.com>
2023-09-07 12:49:44 +0900

    add-youtube

--- allback/pages/index.vue
+++ allback/pages/index.vue
@@ -3,9 +3,21 @@
     <div class="title">
       <h1>鹿追小学校6年生</h1>
       <h2>リコーダー練習用</h2>
+      <div>
+    <iframe
+      width="260"
+      height="200"
+      src="https://www.youtube.com/embed/D6DVTLvOupE?si=7GlxOP7wct_ABgkq"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    ></iframe>
+  </div>
     </div>
     <img class="kyofu" src="~/assets/kyufu-thanks.png" />
   </div>
+  
 </template>
 
 <style scoped>
@@ -13,7 +25,7 @@
     display: flex;
     justify-content: center;
     margin-left: 0;
-    font-family: 'Zen Maru Gothic';
+    font-family: "Zen Maru Gothic";
   }
   .title {
     margin-right: 2rem;
